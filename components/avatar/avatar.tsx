ee3351350ae120d95ab6e2af9b755822fbfa491f
程志国 <zhiguo.cheng@amh-group.com>
2022-03-09 10:51:39 +0800

    chore: 添加avatar sass变量

--- components/avatar/avatar.tsx
+++ components/avatar/avatar.tsx
@@ -1,3 +1,62 @@
-function Avatar(): JSX.Element {
-  return
+import { Image, OpenData, Text, View } from '@tarojs/components'
+import classNames from 'classnames'
+import PropTypes, { InferProps } from 'prop-types'
+import React from 'react'
+import { inWeapp } from '../../common'
+import { AvatarProps } from '../../types/avatar'
+
+const SIZE_CLASS = {
+  mini: 'mini',
+  large: 'large',
+  normal: 'normal',
+  small: 'small',
 }
+function Avatar(props: AvatarProps): JSX.Element {
+  const { size, circle, image, text, openData, customStyle } = props
+  const rootClassName = ['fta-avatar']
+  const iconSize = SIZE_CLASS[size || 'normal']
+  const classObject = {
+    [`fta-avatar--${iconSize}`]: iconSize,
+    'fta-avatar--circle': circle,
+  }
+
+  let letter = ''
+  if (text) letter = text[0]
+
+  let elem: React.ReactNode
+  if (openData && openData.type === 'userAvatarUrl' && inWeapp) {
+    elem = <OpenData type={openData.type}></OpenData>
+  } else if (image) {
+    elem = <Image className='fta-avatar__img' src={image} />
+  } else {
+    elem = <Text className='fta-avatar__text'>{letter}</Text>
+  }
+  return (
+    <View className={classNames(rootClassName, classObject, props.className)} style={customStyle}>
+      {elem}
+    </View>
+  )
+}
+
+const defaultProps: AvatarProps = {
+  size: 'normal',
+  circle: false,
+  text: '',
+  image: '',
+  customStyle: {},
+  className: '',
+}
+
+Avatar.defaultProps = defaultProps
+
+const propTypes: InferProps<AvatarProps> = {
+  size: PropTypes.oneOf(['large', 'normal', 'small', 'mini']),
+  circle: PropTypes.bool,
+  text: PropTypes.string,
+  image: PropTypes.string,
+  openData: PropTypes.object,
+  customStyle: PropTypes.object,
+  className: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+}
+
+Avatar.propTypes = propTypes
--- types/avatar.d.ts
+++ types/avatar.d.ts
@@ -1,8 +1,9 @@
 import { FC } from 'react'
+import BaseComponent from './base'
 
 export type AvatarSize = 'mini' | 'small' | 'normal' | 'large'
 
-export interface AvatarProps {
+export interface AvatarProps extends BaseComponent {
   /**
    * 头像是否圆形
    * mini - 48px
@@ -18,8 +19,20 @@ export interface AvatarProps {
    */
   circle?: boolean
   /**
-   *
+   * 以文字形式展示头像
    */
+  text?: string
+  /**
+   * 头像图片地址
+   */
+  image?: string
+  /**
+   * 参考微信[开放数据](https://developers.weixin.qq.com/miniprogram/dev/component/open-data.html)
+   *
+   * **注意：** openData 仅支持 type 为 userAvatarUrl
+   * @supported weapp
+   */
+  openData?: { type: 'userAvatarUrl' }
 }
 
 declare const Avatar: FC<AvatarProps>
