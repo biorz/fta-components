4d2f4a033f2b1e0cc9e899a50b34da89ebe96f1b
程志国 <zhiguo.cheng@amh-group.com>
2022-03-23 20:07:30 +0800

    chore: result组件标准化

--- components/result/result.tsx
+++ components/result/result.tsx
@@ -1,11 +1,11 @@
-import { Image, Text, View } from '@tarojs/components'
+import { Image, View } from '@tarojs/components'
 import PropTypes, { InferProps } from 'prop-types'
 import React, { Component } from 'react'
-import { Assets, ConfigConsumer, useClassesWithCare } from '../../common'
+import { Assets } from '../../common'
 import '../../style/components/result/index.scss'
 import { ResultProps, ResultType } from '../../types/result'
-import { TouchableOpacity } from '../view'
-
+import Button from '../button'
+import { Text } from '../typography'
 const STATUS: Record<ResultType, ResultProps> = {
   success: {
     src: Assets.tip.success,
@@ -33,38 +33,36 @@ class Result extends Component<ResultProps> {
   public render(): JSX.Element {
     const { type, renderBtn, btnText, onClick } = this.props
     const { src, title, desc } = { ...STATUS[type!], ...this.props }
+
+    const [titleClz, descClz, btnClz] = [
+      'fta-result-title__text',
+      'fta-result-desc__text',
+      'fta-result-button',
+      // 'fta-result-button__text',
+    ]
     return (
-      <ConfigConsumer>
-        {({ careMode }) => {
-          const [titleClz, descClz, btnClz, btnTextClz] = useClassesWithCare.single(
-            careMode,
-            'fta-result-title__text',
-            'fta-result-desc__text',
-            'fta-result-button',
-            'fta-result-button__text'
-          )
-          return (
-            <View className='fta-result'>
-              <Image className='fta-result-image' src={src} mode='aspectFill' />
-              {title ? (
-                <View className='fta-result-title'>
-                  <Text className={titleClz}>{title}</Text>
-                </View>
-              ) : null}
-              {desc ? (
-                <View className='fta-result-desc'>
-                  <Text className={descClz}>{desc}</Text>
-                </View>
-              ) : null}
-              {renderBtn || (
-                <TouchableOpacity className={btnClz} onClick={onClick} activeOpacity={0.6}>
-                  <Text className={btnTextClz}>{btnText}</Text>
-                </TouchableOpacity>
-              )}
-            </View>
-          )
-        }}
-      </ConfigConsumer>
+      <View className='fta-result'>
+        <Image className='fta-result-image' src={src!} mode='aspectFill' />
+        {title ? (
+          <View className='fta-result-title'>
+            <Text level={3} className={titleClz}>
+              {title}
+            </Text>
+          </View>
+        ) : null}
+        {desc ? (
+          <View className='fta-result-desc'>
+            <Text level={4} className={descClz}>
+              {desc}
+            </Text>
+          </View>
+        ) : null}
+        {renderBtn || (
+          <Button className={btnClz} onClick={onClick}>
+            {btnText}
+          </Button>
+        )}
+      </View>
     )
   }
 }
