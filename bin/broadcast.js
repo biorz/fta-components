6dfca60cce637b28ac9fdc084e8a28cb2fe0f9ef
程志国 <zhiguo.cheng@amh-group.com>
2022-04-06 21:44:04 +0800

    chore; 添加发布脚本

--- bin/broadcast.js
+++ bin/broadcast.js
@@ -1,5 +1,7 @@
 const process = require('child_process')
 const version = require('../package.json').version
+const fs = require('fs')
+const { resolve } = require('path')
 
 /**
  * 发布后广播
@@ -11,10 +13,21 @@ function broadcast(version, changelog, isAtAll = false) {
   try {
     process.execSync(`curl 'https://oapi.dingtalk.com/robot/send?access_token=f06d058044555551c9756e7896cacf3bdb6ac3b7e1434efe0274e045e99a6a15' \
     -H 'Content-Type: application/json' \
-    -d '{"msgtype": "text","text": {"content":"FTA View组件库新版本：@fta/components@${version} 已发布\nChangelog: ${changelog}"},"at": {"isAtAll": ${isAtAll}}}'`)
+    -d '{"msgtype": "text","text": {"content":"FTA View组件库新版本：@fta/components@${version} 已发布\n${changelog}"},"at": {"isAtAll": ${isAtAll}}}'`)
   } catch (e) {
     console.log('广播失败')
   }
 }
 
-broadcast(version)
+function readChangelog() {
+  const buffer = fs.readFileSync(resolve('../CHANGELOG.md'))
+  var lines = buffer.toString()
+  const start = lines.indexOf('###')
+  const end = lines.lastIndexOf('###')
+  if (end > start) {
+    var lines = lines.slice(0, end)
+  }
+  return lines.split('\n').slice(5)
+}
+
+broadcast(version, readChangelog(), false)
