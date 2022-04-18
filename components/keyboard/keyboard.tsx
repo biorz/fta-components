d56d32482373ed722a574da32ffb7bdd871d80c9
程志国 <zhiguo.cheng@amh-group.com>
2022-04-18 17:53:15 +0800

    feat(keyboard): 新增键盘组件

--- components/keyboard/keyboard.tsx
+++ components/keyboard/keyboard.tsx
@@ -1,13 +1,14 @@
 import { Text, View } from '@tarojs/components'
-import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
+import React, { useEffect, useMemo, useState } from 'react'
 import { isNumber, isString } from '../../common'
 import '../../style/components/keyboard/index.scss'
-import { KeyboardButtonProps, KeyboardProps, KeyboardType } from '../../types/keyboard'
+import { KeyboardButtonProps, KeyboardProps, KeyboardType, Validator } from '../../types/keyboard'
 import ActionSheet from '../action-sheet'
 
 /** 乱序排列 */
 function disorderList<T = any>(list: T[], flag: boolean) {
-  if (flag) return list
+  if (!flag) return list
   const newer: T[] = []
   const copied = list.slice()
   let len = copied.length
@@ -18,80 +19,156 @@ function disorderList<T = any>(list: T[], flag: boolean) {
   return newer
 }
 
-const baseButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const Validators = {
-  number: /^(([1-9]\d*)|((0|([1-9]\d*))(\.\d+)))$/,
-  id: /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/,
-  phone: /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
-  email: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
-  postcode: /^[1-9]\d{5}(?!\d)$/,
+function validate(value: string, validator: KeyboardProps['validator'], typing = true) {
+  if (!validator) return true
+  if (validator instanceof RegExp) {
+    return validator.test(value)
+  }
+  if (typeof validator === 'function') {
+    return validator(value, typing)
+  }
+  return true
 }
 
-const keyboardTypes: Record<
-  KeyboardType,
-  {
-    validator: KeyboardProps['validator']
-  }
-> = {
-  number: {
-    validator: /1/,
-  },
+const typePresets: KeyboardType[] = ['number', 'decimal', 'id']
+
+const baseButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const presets = {
+  number: [null, 0],
+  decimal: ['.', 0],
+  id: ['X', 0],
 }
+
+const Validators: Record<string, Validator> = {
+  number: /^(([1-9]\d*)|(0))$/,
+  decimal: (value, typing) => {
+    return (
+      typing ? /^(([1-9]\d*)|((0|([1-9]\d*))(\.\d*)))$/ : /^(([1-9]\d*)|((0|([1-9]\d*))(\.\d+)))$/
+    ).test(value)
+  },
+  id: (value, typing) => {
+    return (
+      typing
+        ? /(^\d{0,18}$)|(^\d{17}X?$)/i
+        : /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/
+    ).test(value)
+  },
+  phone: (value, typing) => {
+    return (
+      typing
+        ? /^1\d{0,10}$/
+        : /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/
+    ).test(value)
+  },
+  // email: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+  // postcode: /^[1-9]\d{5}(?!\d)$/,
+}
+
+const InputAdapter = Text
 
 function Keyboard(props: KeyboardProps): JSX.Element {
-  const { type, input, value, controls, validator, disorder, ...actionSheetProps } = props
-  const [val, setVal] = useState<string>(String(value))
+  const {
+    type,
+    showInputBox,
+    value,
+    controlled,
+    validator,
+    disorder,
+    placeholder,
+    maxlength,
+    onChange,
+    onConfirm,
+    customButtons,
+    children,
+    ...actionSheetProps
+  } = props
+  const [val, _setVal] = useState<string>(String(value))
+  const isBasicType = typePresets.includes(type!)
+
+  const setVal = (char: string, index?: number) => {
+    const newVal = val + String(char)
+    if (newVal.length > maxlength!) return
+    if (controlled) return onChange!(newVal, val)
+
+    const _validator = validator || (isBasicType ? Validators[type!] : void 0)
+    if (validate(newVal, _validator)) {
+      _setVal(newVal)
+      onChange!(newVal, val)
+    }
+  }
+
+  const buttons = useMemo(() => {
+    const orderedButtons = isBasicType
+      ? baseButtons.concat(presets[type!] as number[])
+      : type === 'custom'
+      ? customButtons
+      : []
+    // 是否打乱列表
+    const _buttons = disorderList(orderedButtons!, disorder!)
+    // 占位符固定
+    if (type === 'number') {
+      const pidx = _buttons.indexOf(null as unknown as number)
+      _buttons.splice(pidx, 1)
+      return _buttons
+        .slice(0, 9)
+        .concat(null as unknown as number)
+        .concat(_buttons.slice(9))
+    }
+    return _buttons
+  }, [disorder, type])
 
   useEffect(() => {
-    if (controls) {
-      setVal(String(value))
+    if (controlled) {
+      _setVal(String(value))
     }
-  }, [val, controls])
-  // const setVal = (newVal: string) => {
-  //   if (newVal.length < val.length) return _setVal(newVal)
-  //   let valid = true
-  //   if (validator) {
-  //     if (validator instanceof RegExp) {
-  //       valid = validator.test(newVal)
-  //     } else {
-  //       valid = validator(newVal)
-  //     }
-  //   }
-  //   if (valid) _setVal(newVal)
-  // }
+  }, [value, controlled])
+
   const renderDeleteButton = () => (
-    <KeyboardButton onClick={() => setVal(val.slice(0, -1))}>
+    <KeyboardButton
+      onClick={() => {
+        const newVal = val.slice(0, -1)
+        _setVal(newVal)
+        onChange!(newVal, val)
+      }}>
       <DeleteButton />
     </KeyboardButton>
   )
 
+  const inputTextClz = classNames(
+    'fta-keyboard-input__inner-text',
+    placeholder && val === '' && 'fta-keyboard-placeholder'
+  )
+
+  const fullCustom = type === 'custom' && !customButtons?.length
+
   return (
-    <ActionSheet {...actionSheetProps}>
+    <ActionSheet
+      {...actionSheetProps}
+      onConfirm={() => onConfirm!(val, validate(val, validator!, false))}>
       <>
         {/* 显示区 */}
-        {input ? (
+        {showInputBox ? (
           <View className='fta-keyboard-input'>
             <View className='fta-keyboard-input__inner'>
-              <Text className='fta-keyboard-input__inner-text'>{val}</Text>
+              {/* <Text className='fta-keyboard-input__inner-text'>{val}</Text> */}
+              <InputAdapter className={inputTextClz}>{val === '' ? placeholder : val}</InputAdapter>
             </View>
           </View>
         ) : null}
         {/* 键盘区 */}
         <View className='fta-keyboard'>
-          {baseButtons.map((v, i) => (
-            <KeyboardButton
-              key={i}
-              val={v}
-              index={i}
-              onClick={(input) => setVal(val + String(input))}>
-              {v}
-            </KeyboardButton>
-          ))}
-          <KeyboardButton val={'.'} onClick={(input) => setVal(val + String(input))}>
-            .
-          </KeyboardButton>
-          <KeyboardButton val={0} onClick={(input) => setVal(val + String(input))} />
-          {renderDeleteButton()}
+          {fullCustom
+            ? children
+            : buttons.map((v, i) =>
+                v == null ? (
+                  <Placeholder key={i} />
+                ) : (
+                  <KeyboardButton _index={i} key={i} val={v} onClick={setVal}>
+                    {v}
+                  </KeyboardButton>
+                )
+              )}
+          {fullCustom ? null : renderDeleteButton()}
         </View>
       </>
     </ActionSheet>
@@ -102,15 +179,16 @@ function Keyboard(props: KeyboardProps): JSX.Element {
  * 输入按钮
  */
 function KeyboardButton(props: KeyboardButtonProps): JSX.Element {
-  const { onClick, onLongClick, val, index, hoverStyle, children } = props
+  const { onClick, val, _index, hoverStyle, children } = props
 
   return (
     <View
       className='fta-keyboard-item'
       hoverStyle={hoverStyle}
       hoverClass={'fta-keyboard-item--hover'}
-      onClick={() => onClick?.(val, index!)}
-      onLongClick={() => onLongClick?.(val, index!)}>
+      onClick={() => onClick?.(val, _index!)}
+      // onLongClick={() => onLongClick?.(val, _index!)}>
+    >
       {isString(children) || isNumber(children) ? (
         <Text className='fta-keyboard-item__text'>{children}</Text>
       ) : (
@@ -143,18 +221,20 @@ function DeleteButton(): JSX.Element {
 
 const defaultProps: KeyboardProps = {
   value: '',
-  controls: false,
-  input: true,
+  controlled: false,
+  showInputBox: true,
   type: 'number',
+  maxlength: 140,
   title: {
-    title: '数字键盘',
+    title: '请输入',
     cancelText: '取消',
     confirmText: '确定',
   },
   isOpened: true,
   disorder: false,
-  validator: Validators.number,
+  customButtons: [],
   onChange() {},
+  onConfirm() {},
 }
 
 const defaultItemProps: KeyboardButtonProps = {
@@ -167,6 +247,12 @@ KeyboardButton.defaultProps = defaultItemProps
 
 Keyboard.defaultProps = defaultProps
 
-export { DeleteButton, Placeholder, KeyboardButton }
+Keyboard.DeleteButton = DeleteButton
 
-export default Keyboard
+Keyboard.Placeholder = Placeholder
+
+Keyboard.validators = Validators
+
+Keyboard.Button = KeyboardButton
+
+export { Keyboard as default }
--- types/keyboard.d.ts
+++ types/keyboard.d.ts
@@ -1,8 +1,13 @@
-import { CSSProperties, FC } from 'react'
+import { CSSProperties, FC, ReactNode } from 'react'
 import { ActionSheetProps } from './action-sheet'
 import { PropsWithChildren } from './base'
 
 export type KeyboardType = 'id' | 'number' | 'decimal' | 'custom'
+
+/**
+ * typing 为true表示在输入过程中， 为false则表示点击确定按钮时
+ */
+export type Validator = RegExp | ((result: string, typing?: boolean) => boolean)
 
 export interface KeyboardProps extends Partial<ActionSheetProps> {
   /**
@@ -14,42 +19,53 @@ export interface KeyboardProps extends Partial<ActionSheetProps> {
    * 是否是受控组件，不执行校验
    * @default false
    */
-  controls?: boolean
+  controlled?: boolean
   /**
    * 键盘类型
    * @default 'number'
    */
   type?: KeyboardType
   /**
+   * 提示文本
+   */
+  placeholder?: string
+  /**
+   * 可输入的最大长度
+   * @default 140
+   */
+  maxlength?: number
+  /**
    * 是否显示input框展示输入结果
    * @default true
    */
-  input?: boolean
+  showInputBox?: boolean
   /**
    * 校验规则正则或函数
    */
-  validator?: RegExp | ((result: string) => boolean)
+  validator?: Validator
   /**
    * 是否乱序排列
    * @default false
    */
   disorder?: boolean
   /**
+   * 自定义键盘列表，仅在 type='custom'时生效
+   */
+  customButtons?: (string | number)[]
+  /**
+   * 自定义渲染区域，仅在 type='custom' 时生效
+   */
+  children?: ReactNode
+  /**
    * 绑定值改变时
    */
-  onChange?: (newVal: string) => void
+  onChange?: (newVal: string, oldVal: string) => void
+  /**
+   * 点击确定按钮时的回调
+   */
+  onConfirm?: (val: string, valid: boolean) => void
 }
-// Pick<
-// ActionSheetProps,
-// | 'onClose'
-// | 'title'
-// | 'cancelText'
-// | 'confirmText'
-// | 'onCancel'
-// | 'onConfirm'
-// | 'isOpened'
-// | 'useNativeModal'
-// >
+
 export interface KeyboardButtonProps extends PropsWithChildren {
   /**
    * 当前键盘项绑定的值
@@ -62,7 +78,7 @@ export interface KeyboardButtonProps extends PropsWithChildren {
   /**
    * 长按回调
    */
-  onLongClick?: (val: any, index: number) => void
+  // onLongClick?: (val: any, index: number) => void
   /**
    * 按钮激活的样式
    * @supported rn
@@ -72,7 +88,7 @@ export interface KeyboardButtonProps extends PropsWithChildren {
    * 当前键盘项的索引
    * @private
    */
-  index?: number
+  _index?: number
 }
 
 /**
@@ -80,6 +96,28 @@ export interface KeyboardButtonProps extends PropsWithChildren {
  */
 declare const Placeholder: FC<{}>
 
-declare const Keyboard: FC<KeyboardProps>
+/**
+ * 键盘按钮
+ */
+declare const KeyboardButton: FC<KeyboardButtonProps>
+
+/**
+ * 默认的删除按钮
+ */
+declare const DeleteButton: FC<{}>
+
+interface Validators {
+  number: Validator
+  id: Validator
+  decimal: Validator
+  phone: Validator
+}
+
+declare const Keyboard: FC<KeyboardProps> & {
+  Button: typeof KeyboardButton
+  Placeholder: typeof Placeholder
+  DeleteButton: typeof DeleteButton
+  validators: Validators
+}
 
 export default Keyboard
