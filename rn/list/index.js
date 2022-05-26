feff2549195c7d795fa9d6fff1f824fca2f1a995
程志国 <zhiguo.cheng@amh-group.com>
2022-05-26 14:44:33 +0800

    fix(list): 修复list customStyle属性不生效

--- rn/list/index.js
+++ rn/list/index.js
@@ -181,6 +181,20 @@ var indexScssStyleSheet1 = StyleSheet.create({
   },
 })
 
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true,
+    })
+  } else {
+    obj[key] = value
+  }
+  return obj
+}
+
 function _arrayWithHoles(arr) {
   if (Array.isArray(arr)) return arr
 }
@@ -355,6 +369,33 @@ var listItemScssStyleSheet = StyleSheet.create({
   },
 })
 
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object)
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object)
+    enumerableOnly &&
+      (symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable
+      })),
+      keys.push.apply(keys, symbols)
+  }
+  return keys
+}
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = null != arguments[i] ? arguments[i] : {}
+    i % 2
+      ? ownKeys(Object(source), !0).forEach(function (key) {
+          _defineProperty(target, key, source[key])
+        })
+      : Object.getOwnPropertyDescriptors
+      ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source))
+      : ownKeys(Object(source)).forEach(function (key) {
+          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key))
+        })
+  }
+  return target
+}
 function _createSuper$1(Derived) {
   var hasNativeReflectConstruct = _isNativeReflectConstruct$1()
   return function _createSuperInternal() {
@@ -442,7 +483,9 @@ var ListItem = (function (_React$Component) {
           iconInfo = _this$props.iconInfo,
           disabled = _this$props.disabled,
           hasBorder = _this$props.hasBorder,
-          extraThumb = _this$props.extraThumb
+          extraThumb = _this$props.extraThumb,
+          customStyle = _this$props.customStyle,
+          style = _this$props.style
         var _this$props2 = this.props,
           extraText = _this$props2.extraText,
           title = _this$props2.title
@@ -493,16 +536,19 @@ var ListItem = (function (_React$Component) {
               {
                 onClick: _this2.handleClick,
                 hoverStyle: disabled || { backgroundColor: '#F0F0F0' },
-                style: _getStyle$1(
-                  classNames(
-                    rootCareClz,
-                    {
-                      'fta-list__item--multiple': note,
-                      'fta-list__item--disabled': disabled,
-                      'fta-list__item--no-border': !hasBorder,
-                    },
-                    _this2.props.className
-                  )
+                style: _mergeEleStyles(
+                  _getStyle$1(
+                    classNames(
+                      rootCareClz,
+                      {
+                        'fta-list__item--multiple': note,
+                        'fta-list__item--disabled': disabled,
+                        'fta-list__item--no-border': !hasBorder,
+                      },
+                      _this2.props.className
+                    )
+                  ),
+                  _objectSpread(_objectSpread({}, style), customStyle)
                 ),
               },
               React.createElement(
