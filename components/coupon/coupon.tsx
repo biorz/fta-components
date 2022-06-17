8fa7455de0c7f3ec1560e0ed644a3166157e2693
程志国 <zhiguo.cheng@amh-group.com>
2022-06-17 18:49:27 +0800

    chore: 编写优惠券组件

--- components/coupon/coupon.tsx
+++ components/coupon/coupon.tsx
@@ -1,10 +1,115 @@
-import React from 'react'
-import { CouponProps } from '../../types/coupon'
+import { Image, Text, View } from '@tarojs/components'
+import classNames from 'classnames'
+import React, { CSSProperties } from 'react'
+import { isString } from '../../common'
 import '../../style/components/coupon/index.scss'
-import { View } from '@tarojs/components'
+import { CouponProps } from '../../types/coupon'
+
+const IMAGE_PREFIX = 'https://imagecdn.ymm56.com/ymmfile/static/resource/'
+
+const srcset: Record<string, string> = {
+  'rich-unused': IMAGE_PREFIX + '2cb799e7-d6ee-4771-ab63-6a1848692b1d.png',
+  'rich-used': IMAGE_PREFIX + 'ad1e375c-0029-4fdc-b635-e069ae5fc4a8.png',
+  'rich-expired': IMAGE_PREFIX + '697d6222-848a-4205-8535-7de2294cf585.png',
+  'simple-used': IMAGE_PREFIX + 'dc5d4b83-03b6-471f-9567-2399b638bf83.png',
+  'simple-expired': IMAGE_PREFIX + 'c5e8d09e-9f43-44a7-bca6-ad0579ab263a.png',
+}
 
 function Coupon(props: CouponProps) {
-  return <View className='fta-coupon'></View>
+  const {
+    className,
+    customStyle,
+    // @ts-ignore
+    style,
+    status,
+    type,
+    price,
+    meet,
+    title,
+    src,
+    desc,
+    period,
+    remark,
+    remarkBgColor,
+    remarkColor,
+    onBtnClick,
+    btnText,
+  } = props
+
+  const disabled = ['used', 'expired', 'disabled'].includes(status!)
+  const rootClass = classNames('fta-coupon', `fta-coupon--${type}`, className)
+  const rootStyle = { ...style, ...customStyle }
+  const containerClass = classNames('fta-coupon-container', `fta-coupon-coupon--${type}`)
+  const leftClass = classNames('fta-coupon-left', `fta-coupon-left--${type}`)
+  const rightClass = classNames('fta-coupon-right', `fta-coupon-right--${type}`)
+
+  const remarkStyle: CSSProperties = {}
+  let i: any
+  if ((i = remarkBgColor)) remarkStyle.backgroundColor = i
+  if ((i = remarkColor)) remarkStyle.color = i
+
+  return (
+    <View className={rootClass} style={rootStyle}>
+      <View className={containerClass}>
+        <View className={leftClass}>
+          <View className='fta-coupon-price'>
+            <Text className='fta-coupon-price-sign'>￥</Text>
+            <Text className='fta-coupon-price-value'>{price}</Text>
+          </View>
+          {
+            <View className='fta-coupon-meet'>
+              <Text className='fta-coupon-meet__text'>
+                {isString(meet) ? meet : meet! <= 0 ? '无门槛' : `满${meet}元可用`}
+              </Text>
+            </View>
+          }
+        </View>
+        <View className='fta-coupon-line'></View>
+        <View className={rightClass}>
+          <View className='fta-coupon-detail'>
+            <Text className='fta-coupon-title'>{title}</Text>
+            <View
+              className={classNames(
+                'fta-coupon-remark',
+                disabled && 'fta-coupon-remark--disabled'
+              )}>
+              <Text className='fta-coupon-remark__text' style={remarkStyle}>
+                {remark}
+              </Text>
+            </View>
+            <Text className='fta-coupon-period'>{period}</Text>
+          </View>
+          {type === 'rich' ? (
+            <View className='fta-coupon-button' onClick={onBtnClick}>
+              <Text className='fta-coupon-button__text'>{btnText}</Text>
+            </View>
+          ) : (
+            <View className='fta-coupon-radio' />
+          )}
+        </View>
+      </View>
+      {/* 类型为rich独有 */}
+      {type === 'rich' ? (
+        <>
+          <Image src={srcset[`${type}-${status}`]} className='fta-coupon-bg' />
+          <View className='fta-coupon-desc'>
+            <Text className='fta-coupon-desc__text'></Text>
+          </View>
+        </>
+      ) : null}
+    </View>
+  )
 }
+
+const defaultProps: CouponProps = {
+  type: 'simple',
+  price: 50,
+  meet: 150,
+  status: 'unused',
+  btnText: '去使用',
+  remark: '通用券',
+}
+
+Coupon.defaultProps = defaultProps
 
 export default Coupon
--- types/coupon.d.ts
+++ types/coupon.d.ts
@@ -1,35 +1,39 @@
 import { FC } from 'react'
+import BaseComponent from './base'
 
-export type CouponStatus = 'new' | 'unchecked' | 'used' | 'expired' | 'disabled'
+export type CouponStatus = 'unused' | 'used' | 'expired' | 'disabled'
 
-export interface CouponProps {
+export type CouponType = 'simple' | 'rich'
+
+export interface CouponProps extends BaseComponent {
+  type?: CouponType
   /**
    * 优惠券金额
    * @default 50
    */
   price?: number
   /**
-   * 使用条件，满xx元可用
+   * 使用条件，满xx元可用，传入字符串可自定义显示
    * @default 150
    */
-  meet?: number
+  meet?: number | string
   /**
    * 优惠券名称
    */
   title?: string
   /**
    * 优惠券类型
-   * @default '通用'
+   * @default '通用券'
    */
-  type?: string
+  remark?: string
   /**
    * 类型颜色
    */
-  typeColor?: string
+  remarkColor?: string
   /**
    * 类型背景色
    */
-  typeBgColor?: string
+  remarkBgColor?: string
   /**
    * 有效期
    */
@@ -42,6 +46,27 @@ export interface CouponProps {
    * 使用说明
    */
   desc?: string
+  /**
+   * 背景图片
+   */
+  src?: string
+  /**
+   * 展开的使用说明
+   */
+  // extendDesc?: string
+  /**
+   * 点击优惠券的回调
+   */
+  onClick?: () => any
+  /**
+   * 按钮文本，仅在type='rich' && status='unused'时生效
+   * @default '去使用'
+   */
+  btnText?: string
+  /**
+   * 按钮点击回调，仅在type='rich' && status='unused'时生效
+   */
+  onBtnClick?: () => any
 }
 
 declare const Coupon: FC<CouponProps>
